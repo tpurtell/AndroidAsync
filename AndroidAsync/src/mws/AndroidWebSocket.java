989ff48e07440ccabbd888cf298c880a59730820
T.J. Purtell <tpurtell@gmail.com>
2015-05-08 14:28:49 -0700

    remove rc4 ciphers

--- AndroidAsync/src/mws/AndroidWebSocket.java
+++ AndroidAsync/src/mws/AndroidWebSocket.java
@@ -3,8 +3,30 @@ package mws;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.WebSocket;
 import com.koushikdutta.async.http.WebSocket.ConnectCallback;
+import com.koushikdutta.async.http.spdy.SpdyMiddleware;
+import com.koushikdutta.async.http.AsyncSSLEngineConfigurator;
+import com.koushikdutta.async.http.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.net.ssl.SSLEngine;
 
 public class AndroidWebSocket {
+    static {
+        SpdyMiddleware middleware = AsyncHttpClient.getDefaultInstance().getSSLSocketMiddleware();
+        middleware.addEngineConfigurator(new AsyncSSLEngineConfigurator() {
+            @Override
+            public void configureEngine(SSLEngine engine, AsyncHttpClientMiddleware.GetSocketData data, String host, int port) {
+                String[] suites = engine.getEnabledCipherSuites();
+                List<String> filtered = new ArrayList<String>();
+                for(String suite : suites) {
+                    if(suite.toLowerCase().indexOf("rc4") != -1)
+                        continue;
+                    filtered.add(suite);
+                }
+                engine.setEnabledCipherSuites(filtered.toArray(new String[filtered.size()]));
+            }
+        });
+    }
     public static java.util.concurrent.Future<WebSocket> connect(String uri, String protocol,
             WebSocket.ConnectCallback callback) {
         return AsyncHttpClient.getDefaultInstance().websocket(uri, protocol, callback);
